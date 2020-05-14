3bed82be571f888b7fa7748b10a7be5610835bda
KodeStar <kodestar@gmail.com>
2020-05-14 16:25:06 +0100

    add missing username

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -24,6 +24,7 @@ export default {
   no: 'No',
   email: 'Email',
   avatar: 'Avatar',
+  username: 'Username',
   password: 'Password',
   failed: 'Action failed',
   success: 'Action was successful',
